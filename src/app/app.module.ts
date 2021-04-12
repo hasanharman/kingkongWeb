21f7cd5d25ec91a192424953087007e1f464b84a
hasanharman <hasanharman33@gmail.com>
2021-04-12 15:30:40 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
