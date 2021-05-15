7ba6a18f6a6b683cdf96052eb800ef578f5145ab
hasanharman <hasanharman33@gmail.com>
2021-05-15 17:20:36 +0300

    version 2 updates

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -13,13 +13,35 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class FooterComponent implements OnInit {
 	constructor(private modalService: NgbModal) {}
+	sended?: boolean | false;
 
 	ngOnInit(): void {}
 
+	sendButton() {
+		this.sended = true;
+	}
+
+	openWhy(what: any) {
+		this.modalService.open(what, {
+			centered: true,
+			size: 'xl',
+			windowClass: 'dark-modal',
+			backdropClass: 'dark-backdrop'
+		});
+	}
+	openWhat(why: any) {
+		this.modalService.open(why, {
+			centered: true,
+			size: 'xl',
+			windowClass: 'dark-modal',
+			backdropClass: 'dark-backdrop'
+		});
+	}
 	openWeb(web: any) {
 		this.modalService.open(web, {
 			centered: true,
-			size: 'm',
+			scrollable: true,
+			size: 'xl',
 			windowClass: 'dark-modal',
 			backdropClass: 'dark-backdrop'
 		});
@@ -28,19 +50,22 @@ export class FooterComponent implements OnInit {
 	openDev(dev: any) {
 		this.modalService.open(dev, {
 			centered: true,
-			size: 'm',
+			scrollable: true,
+			size: 'xl',
+			windowClass: 'dark-modal',
+			backdropClass: 'dark-backdrop'
+		});
+	}
+	openContact(contact: any) {
+		this.modalService.open(contact, {
+			centered: true,
+			scrollable: true,
+			size: 'xl',
 			windowClass: 'dark-modal',
 			backdropClass: 'dark-backdrop'
 		});
 	}
 	openLegal(legal: any) {
-		// this.modalService.open(legal, {
-		// 	centered: true,
-		// 	size: 'lg',
-		// 	windowClass: 'dark-modal',
-		// 	backdropClass: 'dark-backdrop',
-		// 	scrollable: true
-		// });
 		this.modalService.open(legal, {
 			scrollable: true,
 			centered: true,
--- src/app/shared/slider/slider.component.ts
+++ src/app/shared/slider/slider.component.ts
@@ -10,15 +10,33 @@ declare var $: any;
 })
 export class SliderComponent implements OnInit {
 	activeImg: number = 1;
+	isShown1: boolean = false ;
 	// imgs: Array<string> = ['card1.png','card2.png','card3.png','card4.png'];
 
-	imgs = [
-		{ img: 'card1.png', icon: 'cup.svg', title: 'Kongre / Konferans', sub: '' },
-		{ img: 'card2.png', icon: 'chat.svg', title: 'Dönem Toplantıları', sub:'' },
-		{ img: 'card3.png', icon: 'present.svg', title: 'Sergi / Fuar', sub: '' }
+	imgs1 = [
+		{ img: 'akıllıasistan/akilli-asistan.png', icon: 'akıllıasistan/virtual-assistant-2.svg', title: 'Akıllı Asistan', sub: '' },
+		{ img: 'analitikraporlama/analitik-raporlama.png', icon: 'analitikraporlama/document-copy1.svg', title: 'Analitik Raporlama', sub:'' },
+		{ img: 'anket/anket.png', icon: 'anket/tripod1.svg', title: 'Anket', sub: '' },
+		{ img: 'backpack/backpack.png', icon: 'backpack/multiple-11.svg', title: 'Backpack', sub: '' },
 	];
 
-	totalItems = this.imgs.length;
+	imgs = [
+		{ img: 'DinamikKişiselleştirilmişmarkalama/dinamik-kisisellestirilmis-markalama.png', icon: 'DinamikKişiselleştirilmişmarkalama/billboard1.svg', title: 'Dinamik Kişiselleştirilmiş Markalama', sub:'' },
+		{ img: 'dönemtoplantıları/donem-toplantilari.png', icon: 'dönemtoplantıları/meetingicon.svg', title: 'Dönem Toplantıları', sub: '' },
+		{ img: 'fuarsergi/fuar-sergi.png', icon: 'fuarsergi/fuar-sergi-icon.svg', title: 'Fuar / Sergi', sub: '' },
+		{ img: 'kongre konferans/kongre-konferans.png', icon: 'kongre konferans/kongre-icon.svg', title: 'Kongre / Konferans', sub: '' },
+		{ img: 'Lansman Toplantıları/lansman-toplantilari.png', icon: 'Lansman Toplantıları/lansman-toplantilari-icon.svg', title: 'Lansman Toplantıları', sub:'' },
+		{ img: 'networking seçenekler/networking-secenekleri.png', icon: 'networking seçenekler/hierarchy-55 1.svg', title: 'Networking Seçenekleri', sub: '' },
+		{ img: 'oyun alanları/oyun-alanlari.png', icon: 'oyun alanları/present.svg', title: 'Oyun Alanları', sub: '' },
+		{ img: 'özel odalar/ozel-odalar.png', icon: 'özel odalar/virtual-environment 1.svg', title: 'Özel Odalar', sub: '' },
+		{ img: 'stand alanları/stand-alanlari.png', icon: 'stand alanları/tripod 1.svg', title: 'Stand Alanları', sub:'' },
+		{ img: 'toplantı notları/toplanti-notlari.png', icon: 'toplantı notları/meeting-notes.svg', title: 'Toplantı Notları', sub: '' },
+		{ img: 'yüksek performans/yuksek-performans.png', icon: 'yüksek performans/multiple-11 1.svg', title: 'Yüksek Performans', sub: '' },
+		{ img: 'Zenginleştirilmiş İletişim/zenginlestirilmiş-iletisim.png', icon: 'Zenginleştirilmiş İletişim/voice-record 1.svg', title: 'Zenginleştirilmiş İletişim', sub: '' }
+
+	];
+
+	// totalItems = this.imgs.length;
 
 	constructor(private modals: NgbModal) {}
 
@@ -43,15 +61,19 @@ export class SliderComponent implements OnInit {
 	// 	this.activeImg -= 1;
 	// }
 
-	openSmart(specs: any) {
-		this.modals.open(specs, {
-			scrollable: true,
-			centered: true,
-			size: 'lg',
-			windowClass: 'dark-modal',
-			backdropClass: 'dark-backdrop',
-		});
-		}
+	// openSmart(specs: any) {
+	// 	this.modals.open(specs, {
+	// 		scrollable: true,
+	// 		centered: true,
+	// 		size: 'lg',
+	// 		windowClass: 'dark-modal',
+	// 		backdropClass: 'dark-backdrop',
+	// 	});
+	// 	}
+
+	loadmore() {
+		this.isShown1 = ! this.isShown1;
+	}
 
 	prev() {
 		console.log(this.activeImg);
